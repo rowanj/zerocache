a13e32898b4d341f90e10234e2845ac1e6dbcd19
Rowan James <rowanj@phere.net>
2013-06-08 00:13:50 +1000

    Use dynamic port number and zeroconf discovery

--- src/Client.py
+++ src/Client.py
@@ -1,10 +1,12 @@
 import zmq
+import easyzeroconf
+import time
 
 class Client:
     def __init__(self):
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
-        self.socket.connect('tcp://127.0.0.1:5570')
+        self.socket.setsockopt(zmq.LINGER, 10)
         
     def store(self, key, value):
         print ('sending store(%r:%r)' % (key,value))
@@ -29,10 +31,29 @@ class Client:
         
         return res
 
+    def connect(self, host, port):
+        url = 'tcp://%s:%r' % (host, port)
+        print 'connecting to %r' % (url,)
+        self.socket.connect(url)
+
+def find_servers(client):
+    def callback(host, port):
+        client.connect(host, port)
+
+    easyzeroconf.find('_zerocache._tcp', callback)
+
 def main():
-    client = Client()
-    client.store('8', 'smeee')
-    res = client.fetch('8')
-    print "fetched: %r" % (res,)
+    try:
+        client = Client()
+        find_servers(client)
+        client.store('8', 'smeee')
+        while True:
+            for x in range(10):
+                res = client.fetch('%r' % (x,))
+                print "fetched: %r" % (res,)
+                time.sleep(1)
+
+    except KeyboardInterrupt:
+        pass
 
 main()
--- src/Server.py
+++ src/Server.py
@@ -1,35 +1,50 @@
 import zmq
+import easyzeroconf
+import random
 
 class Server:
     def __init__(self):
         self.store = dict()
+        self.max_items = 1000000
+
+    def shrink(self):
+        self.store.pop(random.choice(self.store.keys))
  
     def doFetch(self, socket, msgKey):
         #print ('doFetch %r' % (msgKey,))
-        msgValue = self.store[msgKey]
-        socket.send(msgKey, zmq.SNDMORE)
-        socket.send(msgValue)
+        if msgKey in self.store:
+            msgValue = self.store[msgKey]
+            socket.send(msgKey, zmq.SNDMORE)
+            socket.send(msgValue)
+        else:
+            socket.send(msgKey)
         
     def doStore(self, socket, msgKey, msgValue):
         #print ('doStore %r:%r' % (msgKey, msgValue))
         self.store[msgKey] = msgValue
         socket.send(msgKey)
+        while len(self.store) > self.max_items:
+            self.shrink()
 
     def run(self):
         context = zmq.Context()
         frontend = context.socket(zmq.REP)
-        frontend.bind('tcp://*:5570')
+        port = frontend.bind_to_random_port('tcp://*', min_port=49152, max_port=65535, max_tries=200)
+        print "bound to port %r" % (port,)
+        easyzeroconf.register('ZeroCache', '_zerocache._tcp', port)
  
-        while True:
-            msgKey = frontend.recv()
-            if (not frontend.getsockopt(zmq.RCVMORE)):
-                self.doFetch(frontend, msgKey)
-            else:
-                msgValue = frontend.recv()
-                self.doStore(frontend, msgKey, msgValue)
+        try:
+            while True:
+                msgKey = frontend.recv()
+                if (not frontend.getsockopt(zmq.RCVMORE)):
+                    self.doFetch(frontend, msgKey)
+                else:
+                    msgValue = frontend.recv()
+                    self.doStore(frontend, msgKey, msgValue)
+        except KeyboardInterrupt:
+            pass
  
         frontend.close()
-        backend.close()
         context.term()
         
 def main():
